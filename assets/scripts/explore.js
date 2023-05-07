5e7a3c8cce23b052f5b057ddd8ba6c12cdb03ddb
amccully <amccully2001@gmail.com>
2023-05-06 21:42:35 -0700

    Expose and Explore complete

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,62 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // constants defined
+  const voicesSelect = document.getElementById('voice-select');
+  const synth = window.speechSynthesis;
+  const button = document.querySelector("button");
+  const img = document.querySelector("[alt='Smiling face']");
+  //
+  
+  // Populate option menu with the list of voices once they have loaded
+  speechSynthesis.onvoiceschanged = () => {
+    const voices = speechSynthesis.getVoices();
+    
+    voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.name;
+      voicesSelect.appendChild(option);
+    });
+  };
+  //
+
+  // On pressing the talk button, the text in the text field will be read by the SpeechSynthesis
+  button.addEventListener("click", (event) => {
+    // get the selected voice
+    const selectedVoice = voicesSelect.value;
+    
+    // get the text to speak
+    const textToSpeak = document.getElementById('text-to-speak').value;
+    
+    // check which voice is selected
+    const voice = synth.getVoices().find((voice) => voice.name === selectedVoice);
+    
+    // create an utterance object with the text to speak and the selected voice
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    utterance.voice = voice;
+    
+    // synthesis will speak the phrase out loud
+    synth.speak(utterance);
+
+    // switch between open and closed smiling face
+    utterance.onstart = onStart;
+    utterance.onend = onEnd;
+
+  });  
+  //
+
+  // When the Synthesis starts speaking, change the image to the open mouth face
+  function onStart(event) {
+    img.src = "assets/images/smiling-open.png";
+  }
+  //
+
+  // When the Synthesis stops speaking, change the image back to the closed mouth face
+  function onEnd(event) {
+    img.src = "assets/images/smiling.png";
+  }
+  //
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,49 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // constants defined
+  const horn = document.getElementById("horn-select");
+  const img = document.querySelector("[alt='No image selected']");
+  const audio = document.querySelector("audio.hidden");
+  const button = document.querySelector("button");
+  const jsConfetti = new JSConfetti();
+  const volume = document.getElementById("volume");
+  const volumeImage = document.querySelector("#volume-controls img");
+  //
+
+  // changing image and sound when horn type is selected
+  horn.addEventListener("change", function() {
+    img.src = `assets/images/${horn.value}.svg`;
+    audio.src = `assets/audio/${horn.value}.mp3`;
+  });
+  //
+
+  // play button for sound is pressed
+  button.addEventListener("click", (event) => {
+    audio.play();
+    if(horn.value == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
+  //
+
+  // for volume adjust
+  volume.addEventListener("change", function() {
+    const volumeValue = volume.value;
+
+    if (volumeValue == 0) {
+      volumeImage.src = "assets/icons/volume-level-0.svg";
+    } else if (volumeValue > 0 && volumeValue < 33) {
+      volumeImage.src = "assets/icons/volume-level-1.svg";
+    } else if (volumeValue >= 33 && volumeValue < 67) {
+      volumeImage.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volumeImage.src = "assets/icons/volume-level-3.svg";
+    }
+  
+    audio.volume = volumeValue / 100;
+  });
+  //
+
 }
